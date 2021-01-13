2be65c6e9819c2cc12c1c06659cf0038c349397c
rammkripa <ram.m.kripa@gmail.com>
2021-01-14 01:03:42 +0530

    read_emdat fixed ver 1.0

--- R/read_emdat.R
+++ R/read_emdat.R
@@ -1,16 +1,15 @@
-read_emdat <- function(path, file_data = TRUE){
-  disaster_data <- readxl::read_excel(path = path,
-                sheet = 1,
+read_emdat <- function(path_to_file, file_data = TRUE){
+  disaster_data <- readxl::read_excel(path = path_to_file,
                 skip = 6,
                 col_names = TRUE)
-  meta_data <- readxl::read_excel(path = path,
-                          sheet = 1,
+  meta_data <- readxl::read_excel(path = path_to_file,
                           n_max = 6,
-                          col_names = FALSE)
+                          col_names = FALSE) %>%
+              rename("Q" = "...1", "A" = "...2")
   return_list <- list()
   if(file_data) {
-    return_list["file_data"] <- meta_data
+    return_list[["file_data"]] <- meta_data
   }
-  return_list["disaster_data"] <- disaster_data
+  return_list[["disaster_data"]] <- disaster_data
   return(return_list)
 }
