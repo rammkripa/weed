65d259ee8448dfe531f8e0778ccffd742cdd088b
rammkripa <ram.m.kripa@gmail.com>
2021-01-14 01:37:54 +0530

    read_emdat 2.0

--- R/read_emdat.R
+++ R/read_emdat.R
@@ -1,3 +1,14 @@
+#' Reads Excel Files obtained from EM-DAT Database
+#' @description EMDAT Database link [here](https://public.emdat.be/)
+#' @param path_to_file A String, the Path to the file downloaded.
+#' @param file_data A Boolean, Do you want information about the file and how it was created?
+#'
+#' @return Returns a list containing one or two tibbles, one for the Disaster Data, and one for File Metadata.
+#' @export
+#'
+#' @examples
+#' read_emdat(path_to_file = here("Tanzania_Kenya2.xlsx"), file_data = TRUE)
+#' @importFrom magrittr %>%
 read_emdat <- function(path_to_file, file_data = TRUE){
   disaster_data <- readxl::read_excel(path = path_to_file,
                 skip = 6,
@@ -5,7 +16,7 @@ read_emdat <- function(path_to_file, file_data = TRUE){
   meta_data <- readxl::read_excel(path = path_to_file,
                           n_max = 6,
                           col_names = FALSE) %>%
-              rename("Q" = "...1", "A" = "...2")
+              dplyr::rename("Q" = "...1", "A" = "...2")
   return_list <- list()
   if(file_data) {
     return_list[["file_data"]] <- meta_data
