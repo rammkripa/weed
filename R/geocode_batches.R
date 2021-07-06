71912327e5872c4f6cd9dc086ce58503f83f5ab5
rammkripa <ram.m.kripa@gmail.com>
2021-07-05 21:09:32 -0500

    docs for new functions ready, 1.0 on track

--- R/geocode_batches.R
+++ R/geocode_batches.R
@@ -1,20 +1,33 @@
 #' Geocode in batches
 #'
+#'
 #' @param . data frame
 #' @param batch_size size of each batch to geocode
 #' @param wait_time in seconds between batches
+#' Note:
+#' default batch_size and wait_time were set to accomplish the geocoding task optimally within the constraints of geonames free access
 #' @param n_results same as geocode
 #' @param unwrap as in geocode
 #' @param geonames_username as in geocode
 #'
-#' @return df but geocoded in batches
+#' @return df geocoded
 #' @export
 #'
 #' @examples
-#' Coming soon
+#' df <- tibble::tribble(
+#'    ~value,  ~location_word,                    ~Country,
+#'    "city of new york",      "new york",                       "USA",
+#'    "mumbai region, district of seattle, sichuan province",  "mumbai","India",
+#'    "mumbai region, district of seattle, sichuan province",  "seattle", "USA",
+#'    "mumbai region, district of seattle, sichuan province", "sichuan",  "China, People's Republic"
+#'    )
+#'
+#' geocode_batches(df, batch_size = 1, wait_time = 4, geonames_username = "rammkripa")
+#'
+#' @importFrom magrittr %>%
 geocode_batches <- function(.,
-                            batch_size,
-                            wait_time,
+                            batch_size = 990,
+                            wait_time = 4800,
                             n_results = 1,
                             unwrap = FALSE,
                             geonames_username) {
@@ -26,7 +39,7 @@ geocode_batches <- function(.,
     print(batch_num)
     start.idx <- batch_num * batch_size
     end.idx <- min( c((batch_num + 1) * batch_size - 1, nrow(df)) )
-    df_slice <- slice(df, start.idx:end.idx)
+    df_slice <- dplyr::slice(df, start.idx:end.idx)
     coded_slice <- df_slice %>%
       geocode(n_results, unwrap, geonames_username)
     listy[[batch_num]] = coded_slice
